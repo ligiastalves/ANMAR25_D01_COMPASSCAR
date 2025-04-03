4fe46f9cdafedf738fda847f06765d4fbec7a896
Ligia Alves <ligia.stalves@gmail.com>
2025-04-03 16:48:05 -0300

    feat: created of the GET method

--- src/controllers/carController.js
+++ src/controllers/carController.js
@@ -1,8 +1,9 @@
 const carService = require("../services/carService");
 const { Car, CarItems } = require('../models');
+const { Op } = require('sequelize');
 
+//route POST
 const createCar = async (req, res) => {
-  console.log("Recebendo requisição POST:", req.body);
   try {
     const car = await carService.createCar(req.body);
     return res.status(201).json(car);
@@ -13,7 +14,7 @@ const createCar = async (req, res) => {
   }
 };
 
-//Update car items
+//route PUT
 const updateCarItems = async (req, res) => {
   try {
     const { id } = req.params;
@@ -49,4 +50,80 @@ const updateCarItems = async (req, res) => {
   }
 };
 
-module.exports = { createCar, updateCarItems };
+//route GET
+const getCarById = async (req, res) =>{
+  try {
+    const { id } = req.params;
+
+    const car = await Car.findByPk(id, {
+      include: {
+        model: CarItems,
+        attributes: ["name"],
+      },
+    });
+
+    if (!car) {
+      return res.status(404).json({ errors: ['car not found'] });
+    }
+
+    const carData = {
+      id: car.id,
+      brand: car.brand,
+      model: car.model,
+      year: car.year,
+      plate: car.plate,
+      created_at: car.created_at,
+      items: car.CarItems ? car.CarItems.map ((item) => item.name) : [],
+    };
+
+    return res.status(200).json(carData);
+  } catch (error){
+    console.error('Error fetching car by ID', error);
+    return res.status(500).json({ errors: ['Internal server error'] });
+  }
+};
+
+//List cars with filters and pagination
+const listCars = async (req, res) => {
+  try {
+    const { year, final_plate, brand, page, limit } = req.query;
+
+    const pageNumber = Math.max(1, parseInt(page) || 1, 1);
+    const limitNumber = Math.min(Math.max(parseInt(limit) || 5, 1), 10);
+    const offset = (pageNumber - 1) * limitNumber;
+
+    const filters = {};
+
+    if (year) {
+      filters.year = { [Op.gte]: parseInt(year) };
+    }
+
+    if (final_plate){
+      filters.plate = { [Op.like]: `%${final_plate}` };
+    }
+
+    if (brand) {
+      filters.brand = {[Op.iLike]: `%${brand}` };
+    }
+
+    const { count, rows } = await Car.findAndCountAll({
+      where: filters,
+      limit: limitNumber,
+      offset: offset,
+      order: [['created_at', 'DESC']],
+    });
+
+    const totalPages = Math.ceil(count / limitNumber);
+    return res.status(200).json({
+      count,
+      pages: totalPages,
+      data: rows,
+    });
+
+  } catch (error){
+    console.error('Error listing cars: ', error);
+    return res.status(500).json ({ errors: ['Internal server error'] });
+  }
+};
+
+module.exports = { createCar, updateCarItems, getCarById, listCars };
--- src/routes/carRoutes.js
+++ src/routes/carRoutes.js
@@ -8,4 +8,10 @@ router.post('/', carController.createCar);
 //Route to upgrade car items
 router.put('/:id/items', carController.updateCarItems);
 
+//Route to get a car by ID
+router.get('/:id', carController.getCarById);
+
+//Router to list cars with pagination and filters
+router.get('/', carController.listCars);
+
 module.exports = router;
